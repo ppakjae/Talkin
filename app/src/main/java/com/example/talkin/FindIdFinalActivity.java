03e6db1114fe70012a663072fd03d6dc7f5eec9f
Jaesun Park <zzangjames75@gmail.com>
2019-05-11 16:36:17 +0900

    add find_id layout

---
+++ app/src/main/java/com/example/talkin/FindIdFinalActivity.java
@@ -0,0 +1,15 @@
+package com.example.talkin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FindIdFinalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_id_final);
+
+
+    }
+}
