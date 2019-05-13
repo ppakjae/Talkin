f74e48ae428771004005e8a96588570ec55ba531
LeeDonggun <tnpdlzi@naver.com>
2019-05-13 21:22:10 +0900

    add FindPwActivity

--- app/src/main/java/com/example/talkin/FindPwActivity.java
+++ app/src/main/java/com/example/talkin/FindPwActivity.java
@@ -2,6 +2,7 @@ package com.example.talkin;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,8 +16,6 @@ public class FindPwActivity extends AppCompatActivity {
     EditText edtId;
     EditText edtMail;
     EditText edtRecognize;
-
-    //수정
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +31,38 @@ public class FindPwActivity extends AppCompatActivity {
         edtMail = (EditText) findViewById(R.id.edtMail);
         edtRecognize = (EditText) findViewById(R.id.edtRecognize);
 
+        btnIdCheck.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                IdCheckDialog idCheckDialog = new IdCheckDialog(FindPwActivity.this);
+                idCheckDialog.show();
+            }
+        });
+
+        btnMailCheck.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                EmailVerifyDialog emailVerifyDialog = new EmailVerifyDialog(FindPwActivity.this);
+                emailVerifyDialog.show();
+            }
+        });
+
+
+        btnMailRecognize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                if(true) {
+//                    EmailVerifyDialog emailVerifyDialog = new EmailVerifyDialog(FindIdActivity.this, "인증 실패" ,"인증번호를 다시 확인해주세요." );
+//                    emailVerifyDialog.setImgMail();
+//                    emailVerifyDialog.show();
+//                }
+//                else{
+                EmailVerifyDialog emailVerifyDialog = new EmailVerifyDialog(FindPwActivity.this, "인증 완료" ,"이메일 인증이 완료되었습니다." );
+                emailVerifyDialog.show();
+
+//                }
+            }
+        });
 
     }
 }
