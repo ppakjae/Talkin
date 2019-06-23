f50d348dfb90dad528f8ac5704302296b8d2e95c
박민규 <pmk9440@naver.com>
2019-06-23 20:25:57 +0900

    과제하는중

--- app/src/main/java/com/example/talkin/MainpageActivity.java
+++ app/src/main/java/com/example/talkin/MainpageActivity.java
@@ -1,7 +1,15 @@
 package com.example.talkin;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainpageActivity extends AppCompatActivity {
 
@@ -9,5 +17,11 @@ public class MainpageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
+    }
+
+    public void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
     }
 }
