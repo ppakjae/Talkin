703bfa6724785218bfbe797f6d72379e0df84963
LeeDonggun <tnpdlzi@naver.com>
2019-06-24 16:36:48 +0900

    ㅇㅇ

---
+++ app/src/main/java/com/example/talkin/TeamActivity.java
@@ -0,0 +1,11 @@
+package com.example.talkin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+
+public class TeamActivity extends AppCompatActivity  {
+    EditText edtTitle = (EditText) findViewById(R.id.edtTitle);
+
+
+}
